86c9fe9346fe8363691cb9098e489493c07f6617
Vaibhav Thakur <vaibhav.s.thakur@gmail.com>
2022-10-17 12:13:19 +0530

    First commit

---
+++ src/main/java/com/javaminds/thecodemine/java8/ForEachDemo.java
@@ -0,0 +1,36 @@
+package com.javaminds.thecodemine.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ForEachDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(1,2,3,6,5);
+
+        System.out.println("Normal for loop");
+        for (int i = 0; i < nums.size(); i++) {
+            System.out.println(nums.get(i));
+        }
+
+        System.out.println("Enhanced for loop");
+        for(int i: nums){
+            System.out.println(i);
+        }
+
+        System.out.println("for loop using Java 8");
+        Consumer<Integer> consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        };
+        nums.forEach(consumer);
+
+        System.out.println("for loop using Java 8 Lambda exp");
+        nums.forEach(n -> System.out.println(n));
+
+    }
+}
