d35c996c69f6e83721fcd7524c7b0045f7c69b5e
Vaibhav Thakur <vaibhav.s.thakur@gmail.com>
2022-10-17 12:21:53 +0530

    second commit

--- src/main/java/com/javaminds/thecodemine/java8/ForEachDemo.java
+++ src/main/java/com/javaminds/thecodemine/java8/ForEachDemo.java
@@ -9,7 +9,6 @@ public class ForEachDemo {
     public static void main(String[] args) {
 
         List<Integer> nums = Arrays.asList(1,2,3,6,5);
-
         System.out.println("Normal for loop");
         for (int i = 0; i < nums.size(); i++) {
             System.out.println(nums.get(i));
